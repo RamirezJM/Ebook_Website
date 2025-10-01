f79e5e0a77984106a8837f39fe4339c5dacadfdf
RamirezJM <165516814+RamirezJM@users.noreply.github.com>
2025-09-30 21:14:48 -0300

    page index

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+function userScroll() {
+  const navBar = document.querySelector('.navbar')
+  window.addEventListener('scroll', () =>{
+    if(window.scrollY > 50) {
+      navBar.classList.add('bg-dark');
+      navBar.classList.add('navbar-sticky')
+    }
+    else{
+      navBar.classList.remove('bg-dark')
+      navBar.classList.remove('navbar-sticky')
+    }
+  } )
+}
+
+document.addEventListener('DOMContentLoaded', userScroll)
+
